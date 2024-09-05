a04e00e60f44afd6fed7c9c69c2f8b743ab06931
Taylover <2236373829@qq.com>
2024-09-05 21:59:33 +0800

    员工分页查询业务代码

--- sky-take-out/sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -19,4 +21,11 @@ public interface EmployeeService {
      * @return
      */
     boolean save(EmployeeDTO employeeDTO);
+
+    /**
+     * 根据条件分页查询员工信息
+     * @param queryDTO
+     * @return
+     */
+    PageResult page(EmployeePageQueryDTO queryDTO);
 }
