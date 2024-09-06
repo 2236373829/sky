0340f2915336843a1e75773b29c83bec6129f5d0
Taylover <2236373829@qq.com>
2024-09-06 22:23:28 +0800

    使用aop将公共字段自动填充

---
+++ sky-take-out/sky-server/src/main/java/com/sky/annotation/AutoFIll.java
@@ -0,0 +1,5 @@
+package com.sky.annotation;/**
+    @author xyzZero3
+    @date 2024/9/6 16:10
+*/public @interface AutoFIll {
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,5 @@
+package com.sky.aspect;/**
+    @author xyzZero3
+    @date 2024/9/6 17:09
+*/public class AutoFillAspect {
+}
