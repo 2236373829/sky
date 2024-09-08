7280d6b1aa7eaf152ab4c2f80f70015982c735e6
Taylover <2236373829@qq.com>
2024-09-08 23:12:44 +0800

    菜品接口开发

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -79,4 +79,16 @@ public interface DishMapper {
      */
     @AutoFIll(OperationType.UPDATE)
     int update(Dish dish);
+
+    /**
+     * 根据分类id查询菜品
+     *
+     * List<Dish>
+     * @param categoryId
+     * @return
+     */
+    @Select("select id, name, category_id, price, image, description, status, create_time, update_time, create_user, update_user " +
+            "from dish where category_id = #{categoryId} and status = 1")
+    List<Dish> list(Long categoryId);
+
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -154,4 +154,15 @@ public class DishServiceImpl implements DishService {
 
         return false;
     }
+
+    /**
+     * 根据分类id查询菜品
+     *
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public List<Dish> list(Long categoryId) {
+        return dishMapper.list(categoryId);
+    }
 }
