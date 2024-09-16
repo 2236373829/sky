6255426683426942c2c6bb1449daefa4f48e8002
Taylover <2236373829@qq.com>
2024-09-16 16:47:41 +0800

    客户端查询订单详情功能开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -5,6 +5,7 @@ import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 
 /**
  * @author xyzZero3
@@ -42,6 +43,14 @@ public interface OrderService {
      * @return
      */
     PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    /**
+     * 查询订单详情
+     *
+     * @param id
+     * @return
+     */
+    OrderVO details(Long id);
 
     /**
      * 客户催单
